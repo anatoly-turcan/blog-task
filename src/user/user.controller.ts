f589b3d86148946032a90fc10b48dbdeb519ddd0
Anatoly Turcan <anatoly@turcan.dev>
2023-02-12 16:00:04 +0200

    feat: add endpoint to get self user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { User } from './decorators/user.decorator';
+import UserEntity from './entities/user.entity';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get()
+  getMe(@User() user: UserEntity) {
+    return user;
+  }
 }
